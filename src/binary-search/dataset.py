9d6f275a878550a4668caaa998272206be8cc0a9
DrizztLU <dylanzelmar@gmail.com>
2020-10-10 01:25:59 +0200

    modified with brain on

--- src/binary-search/dataset.py
+++ src/binary-search/dataset.py
@@ -3,10 +3,10 @@ import random
 # will generate either an odd or even dataset
 def generateDataset():
     _dataset = []
-    _theStartingPoint = random.randint(1,2)
+    _data = random.randint(1,2)
 
     for i in range(100):
-        _dataset.append(i)
-        i += 2
+        _dataset.append(_data)
+        _data += 2
 
     return _dataset
