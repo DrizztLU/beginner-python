4e79b5e6bd6c02b4b9ec1f587738ee713f1ef43d
DrizztLU <dylanzelmar@gmail.com>
2020-10-11 12:51:01 +0200

    CSV Parser

--- src/extract-data/main.py
+++ src/extract-data/main.py
@@ -1,4 +1,5 @@
 from logic.jsonParser import JsonParser
+from logic.csvParser import CSVParser
 from time import perf_counter 
 
 theTimers = {
@@ -16,4 +17,15 @@ theJsonTimerStop = perf_counter()
 
 theTimers["json"] = theJsonTimerStop - theJsonTimerStart
 
-print ("JSon parse elapsed time: "+ theTimers["json"] + "s")
+print ("JSon parse elapsed time: "+ str(theTimers["json"]) + "s")
+
+theCSVTimerStart = perf_counter()
+
+theCSVParser = CSVParser()
+theCSVData = theCSVParser.ParseFiles()
+
+theCSVTimerStop = perf_counter()
+
+theTimers["csv"] = theCSVTimerStop - theCSVTimerStart
+
+print ("CSV parse elapsed time: "+ str(theTimers["csv"]) + "s")
