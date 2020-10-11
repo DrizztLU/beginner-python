e5d7413be9f3d5fc657d8328eb088e637f8ad59a
DrizztLU <dylanzelmar@gmail.com>
2020-10-11 17:13:40 +0200

    XML parser

--- src/extract-data/logic/xmlParser.py
+++ src/extract-data/logic/xmlParser.py
@@ -1,7 +1,9 @@
 # xmlParser.py
 from logic.abstractParser import AbstractParser
+from xml.etree import ElementTree
+from model.xmlNodeTypeEnum import XMLNodeTypeEnum
 
-class CSVParser(AbstractParser):
+class XMLParser(AbstractParser):
     # Constants
     cFileExtension = "xml"
 
@@ -14,9 +16,47 @@ class CSVParser(AbstractParser):
         for theFilePath in self.Files:
 
             with open(theFilePath, encoding='utf-8-sig') as theFile:
+                theTree = ElementTree.parse(theFile)
+                theRoot = theTree.getroot()
                 
-                
+                theData = self.__BuildData(theRoot)
 
                 theResult.append(theData)
 
         return theResult
+
+    def __BuildData(self, pNode):
+        theNodeType = self.__DetermineNodeType(pNode)
+
+        theArrayIteration = 0
+
+        if theNodeType == XMLNodeTypeEnum.SIMPLEOBJECT:
+            return pNode.text
+
+        elif theNodeType == XMLNodeTypeEnum.OBJECT:
+            theData = {}
+            for theNode in list(pNode):
+                theData[theNode.tag] = self.__BuildData(theNode) 
+
+            return theData
+
+        elif theNodeType == XMLNodeTypeEnum.ARRAY:
+            theData = {}
+            theData[pNode.tag] = []
+            for theNode in list(pNode):
+                theData[pNode.tag].append(self.__BuildData(theNode))
+            return theData
+
+
+    def __DetermineNodeType(self, pNode):
+        theChildren = list(pNode)
+
+        if len(theChildren) == 0:
+            return XMLNodeTypeEnum.SIMPLEOBJECT
+        elif (len(theChildren) > 1 and theChildren[0].tag == theChildren[1].tag) or pNode.tag == theChildren[0].tag + 's':
+            return XMLNodeTypeEnum.ARRAY
+        else:
+            return XMLNodeTypeEnum.OBJECT
+
+        # unreachable code 
+        return XMLNodeTypeEnum.NONE
